0adcdc204086ceb2ecc2eaf3012ade55b88b9ebb
Keizo Fujiwara <keizo@pyull.com>
2014-08-14 08:13:47 +0900

    Consolidate YahooMapAPI.cs into BackgroundTasks (2)
    - Change YahooMapAPI to sealed.
    - Actually delete YahooMapAPI.cs from the main project

--- BackgroundTasks/YahooMapAPI.cs
+++ BackgroundTasks/YahooMapAPI.cs
@@ -4,14 +4,17 @@ using System.Text;
 
 namespace BackgroundTasks
 {
-    public class YahooMapAPI
+    public sealed class YahooMapAPI
     {
-        public static string ApplicationID = <Place your app id>;
+        public static string GetApplicationID()
+        {
+            return <Replace with AppID>;
+        }
 
         public static string GenerateStaticMapURL(int width, int height, double lat, double lon, int zoom)
         {
             return string.Format("http://map.olp.yahooapis.jp/OpenLocalPlatform/V1/static?appid={0}&lat={1}&lon={2}&z={3}&width={4}&height={5}&overlay=type:rainfall|datelabel:on",
-                ApplicationID,
+                GetApplicationID(),
                 lat,
                 lon,
                 zoom,
