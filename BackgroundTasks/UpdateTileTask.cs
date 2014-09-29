34cdf5e20731454ba9ccc150f2f93266de27ef5c
Keizo Fujiwara <keizo@pyull.com>
2014-09-29 14:43:51 +0900

    Update Lat/Lon/Zoom of tile to map center

--- BackgroundTasks/UpdateTileTask.cs
+++ BackgroundTasks/UpdateTileTask.cs
@@ -22,12 +22,27 @@ namespace BackgroundTasks
             var tileUpdater = Windows.UI.Notifications.TileUpdateManager
                   .CreateTileUpdaterForApplication();
 
+            double lat = 35.681093831866455;
+            double lon = 139.76716278230535;
+            int zoom = 8;
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (localSettings.Values.ContainsKey("TileLatitude") == true
+                && localSettings.Values.ContainsKey("TileLongtitude") == true
+                && localSettings.Values.ContainsKey("TileZoom") == true)
+            {
+                lat = (double)localSettings.Values["TileLatitude"];
+                lon = (double)localSettings.Values["TileLongtitude"];
+                zoom = (int)localSettings.Values["TileZoom"];
+            }
+
             var tile310x310 = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare310x310Image);
-            tile310x310.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(310, 310, 35.681093831866455, 139.76716278230535, 8);
+            tile310x310.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(310, 310, lat, lon, zoom);
             var tile310x150 = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Image);
-            tile310x150.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(310, 150, 35.681093831866455, 139.76716278230535, 8);
+            tile310x150.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(310, 150, lat, lon, zoom);
             var tile150x150 = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Image);
-            tile150x150.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(150, 150, 35.681093831866455, 139.76716278230535, 8);
+            tile150x150.GetElementsByTagName("image")[0].Attributes.GetNamedItem("src").InnerText = YahooMapAPI.GenerateStaticMapURL(150, 150, lat, lon, zoom);
 
             // Clear previously set tiles
             tileUpdater.Clear();
--- NimbusRadar/NimbusRadar.Windows/MainPage.xaml.cs
+++ NimbusRadar/NimbusRadar.Windows/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,10 @@ namespace NimbusRadar
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private double _receivedLatitude;
+        private double _receivedLongitude;
+        private int _receivedZoom;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -32,6 +37,50 @@ namespace NimbusRadar
             LiveTiles.AddLiveTiles();
 
             YahooJSMapWebView.Source = new Uri("ms-appx-web:///Assets/YahooJSMap.html");
+        }
+
+        private async void ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            if (e.Value.StartsWith("latitude"))
+            {
+                char[] separators = { ':' };
+                this._receivedLatitude = Double.Parse(e.Value.Split(separators)[1]);
+            }
+
+            if (e.Value.StartsWith("longtitude"))
+            {
+                char[] separators = { ':' };
+                this._receivedLongitude = Double.Parse(e.Value.Split(separators)[1]);
+            }
+
+            if (e.Value.StartsWith("zoom"))
+            {
+                char[] separators = { ':' };
+                this._receivedZoom = Int32.Parse(e.Value.Split(separators)[1]);
+            }
+
+            if (e.Value == "updateTilePositionButton")
+            {
+                MessageDialog dialog = new MessageDialog("地図の中心部分をタイルに表示します");
+                dialog.Commands.Add(new UICommand("OK", new UICommandInvokedHandler(DialogCommandhandler)));
+                dialog.Commands.Add(new UICommand("キャンセル", new UICommandInvokedHandler(DialogCommandhandler)));
+
+                dialog.DefaultCommandIndex = 1;
+                dialog.CancelCommandIndex = 2;
+
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void DialogCommandhandler(IUICommand command)
+        {
+            if (command.Label == "OK")
+            {
+                var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+                localSettings.Values["TileLatitude"] = this._receivedLatitude;
+                localSettings.Values["TileLongtitude"] = this._receivedLongitude;
+                localSettings.Values["TileZoom"] = this._receivedZoom;
+            }
         }
     }
 }
